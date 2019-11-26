0596d18db7b22aa6d7817e5e667af959451db313
celina-src <58207140+celina-src@users.noreply.github.com>
2019-11-26 17:11:10 +0100

    Update Stairs.java

--- src/exit/Stairs.java
+++ src/exit/Stairs.java
@@ -1,14 +1,20 @@
 package exit;
 
-public class Stairs extends Exit {
+import place.Place;
+
+public class Window extends Exit {
+
+	public String toString() {
+		// TODO - implement Window.toString
+		throw new UnsupportedOperationException();
+	}
 
 	/**
 	 * 
 	 * @param P1
 	 * @param P2
 	 */
-	public Stairs(Place P1, Place P2) {
-		// TODO - implement Stairs.Stairs
-		throw new UnsupportedOperationException();
+	public Window(Place P1, Place P2) {
+            super(P1, P2);
 	}
 }
