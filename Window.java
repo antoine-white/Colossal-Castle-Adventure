70a8ace158dc813065c93532054f3a2f9007bd6a
celina-src <58207140+celina-src@users.noreply.github.com>
2019-11-26 16:43:28 +0100

    Add files via upload

---
+++ Window.java
@@ -0,0 +1,19 @@
+package exit;
+
+public class Window extends Exit {
+
+	public String toString() {
+		// TODO - implement Window.toString
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param P1
+	 * @param P2
+	 */
+	public Window(Place P1, Place P2) {
+		// TODO - implement Window.Window
+		throw new UnsupportedOperationException();
+	}
+}
