350dfbb1e23323e6503a1c31791afac2a04b6547
antoine-white <antoine.maxime.bl@gmail.com>
2019-11-26 16:46:46 +0100

    add a test method

--- tests/items/ItemIT.java
+++ tests/items/ItemIT.java
@@ -4,6 +4,18 @@ package items;
     import static org.junit.Assert.*;
     
     public class ItemIT {
+        @Test
         
+        public void getNameTest(){
+            String name1 = "test";
+            String name2 = "";            
+            Item i = new Item(name1,"");
+            Item i2 = new Item(name2,"");            
+            Item i3 = new Item(null,"");
+            assertEquals(name1,i.getNAME());
+            assertEquals(name2,i2.getNAME());
+            assetNull(i3.getNAME());
+        }
+
     
     }
