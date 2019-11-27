9d604a079d94f1548bee7f7371ede1de6e84196d
celina-src <58207140+celina-src@users.noreply.github.com>
2019-11-27 10:22:11 +0100

    Update HiddenRoom.java

--- src/place/HiddenRoom.java
+++ src/place/HiddenRoom.java
@@ -1,20 +1,23 @@
 package place;
 
+import interactions.*;
+import storage.Storage;
+
 public class HiddenRoom extends Room {
 
 	private static String DEFAULT_DESCRIPTION;
 
 	/**
-	 * 
-	 * @param name
-	 * @param level
-	 * @param storages
-	 * @param paints
-	 * @param ennemies
-	 */
-	public HiddenRoom(String name, int level, Storage[] storages, TalkPaint[] paints, Ennemy[] ennemies) {
-		// TODO - implement HiddenRoom.HiddenRoom
-		throw new UnsupportedOperationException();
+         * 
+         * @param description
+         * @param name
+         * @param level
+         * @param store
+         * @param paints
+         * @param ennemies 
+         */
+	public HiddenRoom(String description, String name, int level, 
+                Storage store, TalkPaint paints, Ennemy ennemies) {
+            super(description, name, level, store, paints, ennemies);
 	}
-
 }
