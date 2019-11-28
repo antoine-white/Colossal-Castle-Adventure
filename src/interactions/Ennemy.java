67d8619b190566434cead9f6be56cf0db227cd84
antoine-white <antoine.maxime.bl@gmail.com>
2019-11-28 14:40:41 +0100

    add to string in absctract

--- src/interactions/Ennemy.java
+++ src/interactions/Ennemy.java
@@ -2,52 +2,56 @@ package interactions;
 
 public abstract class Ennemy {
 
-	private int hp;
-	private static int DEFAULT_HP;
-	private static int DEFAULT_DAMAGE;
-	private int damage;
+    private int hp;
+    private static int DEFAULT_HP;
+    private static int DEFAULT_DAMAGE;
+    private int damage;
 
-        /**
-         * 
-         */
-	public Ennemy() {
-            this.hp = DEFAULT_HP; 
-            this.damage = DEFAULT_DAMAGE; 
-	}
+    /**
+     * 
+     */
+    public Ennemy() {
+        this.hp = DEFAULT_HP;
+        this.damage = DEFAULT_DAMAGE;
+    }
 
-        /**
-         * 
-         * @param hp
-         * @param damage 
-         */
-        public Ennemy(int hp, int damage) {
-            this.hp = hp; 
-            this.damage = damage; 
-            
-            throw new UnsupportedOperationException();
-	}
-        
-        /**
-         * 
-         * @return 
-         */
-        public int attack() { 
-            if(this.hp > 0){
-              return this.damage;   
-            } else {
-                return 0; 
-            }    
+    /**
+     * 
+     * @param hp
+     * @param damage
+     */
+    public Ennemy(int hp, int damage) {
+        this.hp = hp;
+        this.damage = damage;
+
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public int attack() {
+        if (this.hp > 0) {
+            return this.damage;
+        } else {
+            return 0;
         }
+    }
 
-	/**
-	 * 
-	 * @param damage
+    /**
+     * 
+     * @param damage
      * @return the number of hp remaining after the attack
-	 */
-	public int attacked(int damage) {  
-            this.hp -= damage; 
-            return this.hp;
-	}
+     */
+    public int attacked(int damage) {
+        this.hp -= damage;
+        return this.hp;
+    }
 
-        
+    @Override
+    abstract public String toString();
+
+    
+
 }
