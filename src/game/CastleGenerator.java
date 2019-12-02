d6ed8349031990c97f868e18f10b40adb0e3c02e
Med-z <55351063+Med-z@users.noreply.github.com>
2019-12-02 09:42:10 +0100

    rdc terminé

--- src/game/CastleGenerator.java
+++ src/game/CastleGenerator.java
@@ -12,68 +12,83 @@ import items.*;
  */
 public class CastleGenerator {
 
-    /**
-     * generate a default game 
-     */
-    public static Room generateCastle(){
-        //                                         DEMANDER A ANTOINE POUR LES STORAGES 
-
-        // Initialisation des objets et ennemi ( degat et volume arbitraire)
-        Armor ArmorEnnemy = new Armor(70,25); 
-        Skeleton skeletonEnnemy = new Skeleton(50,20); 
-        Stick StickWeapon = new Stick(15,5,"Baton","Baton qui sert d'armes"); 
-        Axe AxeWeapon = new Axe(20,5,"Hache","Hache servant d'armes"); 
+    
+    private static Trunk generateTrunk(TakeableItem i){
+        Trunk t = new Trunk(); 
+        t.addItem(i); 
+        return t; 
+    }
         
-
+    private static Trunk generateTrunk(TakeableItem[] tab){
+        Trunk t = new Trunk(); 
+        for(TakeableItem i : tab){
+            t.addItem(i); 
+        }
+        return t; 
+    }
+    
+    public static Room generateCastle(){
         /***************  REZ DE CHAUSSEE DU CHATEAU *******************/
         
-        // Piece d'arrivée le hall, sans rien 
-        Room Entrance = new Room("Entree du chateau ","Entrée",0,null,null); 
+        // Piece d'arrivée le hall, sans rien
+        Room Entrance = new Room("Castle's Entrance ","entrance",0,null,null); 
 
         // Salle a manger sans coffre ni ennemi 
-        Room DiningRoom = new Room("Salle a manger près de l'entrée du chateau","Salle A Manger",0,null,null); 
+        Room DiningRoom = new Room("The dining room next the entrance","diningroom",0,null,null); 
 
         // Cuisine avec coffre sans ennemi
-        Trunk KitchenTrunk = new Trunk(); // ------> Arme dans celui la 
-        Room Kitchen = new Room("Cuisine près de l'entrée du chateau","Cuisine",0,KitchenTrunk,null);
+        Trunk KitchenTrunk = new Trunk(); // ------> Arme (Baton) dans celui la 
+        KitchenTrunk.addItem(new Stick(20,5,"Stick","Stick which can be used as weapon")); 
+        Room Kitchen = new Room("Kitchen next the entrance","kitchen",0,KitchenTrunk,null);
 
-                                                        // rajouter piece inconnue 
-
-        // Sellier sans coffre avec ennemi armure 
-        Room Cellar = new Room("Cellier près de l'entrée du chateau","Cellier",0,null,ArmorEnnemy);
+        // Cellier sans coffre avec ennemi armure 
+        Room Cellar = new Room("Cellar next the kitchen","cellar",0,null,new Armor(70,10));
 
         // salle de bal avec coffre normal et sans ennemi
         Trunk BalTrunk = new Trunk(); // ------> Clé dans celui la 
-        Room BallRoom = new Room("Salle de bal près de l'entrée du chateau","Salle de Bal",0,BalTrunk,null); 
+        BalTrunk.addItem(new DoorKey("Key","Key which can open a locked door")); 
+        Room BallRoom = new Room("Ballroom next the entrance of the castle","ballroom",0,BalTrunk,null); 
 
-        // Taverne sans coffre et sans ennemie 
-        Room Tavern = new Room("Ancienne Taverne du chateau","Taverne",0,null,null); 
+        // Taverne sans coffre et sans ennemi
+        Room Tavern = new Room("An old tavern","tavern",0,null,null); 
 
         // Terrasse avec coffre et sans ennemi 
         Trunk TerraceTrunk = new Trunk(); // ------> Objet de Soin dans celui la 
-        Room Terrace = new Room("Terrace près de la salle de bal","Terrasse",0,TerraceTrunk,null); 
+        TerraceTrunk.addItem(new HealObject(20,5,"HealObject","An item which can heal you")); 
+        Room Terrace = new Room("Terrace next the ballroom","Terrace",0,TerraceTrunk,null); 
+        
+        // Jardin d'hiver sans coffre et avec ennemi 
+        Room WinterGarden = new Room("A Winter garden next the entrance","wintergarden",0,null,new Skeleton(50,20)); 
 
             /** PORTES **/
-        //Porte reliant salle a manger et entrée 
-        Door Entrance_DiningRoom = new Door(Entrance,DiningRoom); 
-        //Porte reliant entrée et salle de bal 
+        Door Entrance_DiningRoom = new Door(Entrance,DiningRoom);  
         Door Entrance_BallRoom = new Door(Entrance,BallRoom); 
-        //Porte reliant Salle de Bal et Taverne 
+        Door Entrance_WinterGarden = new Door(Entrance,WinterGarden); 
         Door Tavern_BallRoom = new Door(BallRoom,Tavern); 
-        // Porte reliant cuisine et salle a manger 
         Door DiningRoom_Kitchen = new Door(DiningRoom,Kitchen); 
-        // Porte reliant cellier à la cuisine 
         Door Kitchen_Cellar = new Door(Kitchen,Cellar); 
-
+       
+            
+           /** SORTIES **/ 
+            
         Entrance.setExit(Entrance_DiningRoom);
-        Entrance.setExit(Entrance_BallRoom);
         DiningRoom.setExit(Entrance_DiningRoom);
-        DiningRoom.setExit(DiningRoom_Kitchen);
+        
+        Entrance.setExit(Entrance_BallRoom);
         BallRoom.setExit(Entrance_BallRoom);
-        BallRoom.setExit(Tavern_BallRoom);
-        Kitchen.setExit(Kitchen_Cellar);
+        
+        Entrance.setExit(Entrance_WinterGarden); 
+        WinterGarden.setExit(Entrance_WinterGarden);
+        
+        DiningRoom.setExit(DiningRoom_Kitchen);
         Kitchen.setExit(DiningRoom_Kitchen);
+        
+        BallRoom.setExit(Tavern_BallRoom);
+        Tavern.setExit(Tavern_BallRoom); 
+        
+        Kitchen.setExit(Kitchen_Cellar);
         Cellar.setExit(Kitchen_Cellar);
+        
            /** ESCALIERS  **/
         
         return Entrance;
