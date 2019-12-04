315e8b9e162848058cb9e1c5f4a85f5730234606
antoine-white <40806880+antoine-white@users.noreply.github.com>
2019-12-04 15:11:40 +0100

    add a goal to the game

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,8 @@ import game.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome in our game Collosal Castle Adventure.\nUse HELP command to get all available command\n\n");
+        System.out.println("Welcome in our game Collosal Castle Adventure.\nUse HELP command to get all available command\n"
+                           "You are an explorer in a massive haunted castle looking for a treasure...\n");
         Game g = new Game();
         g.runGame();        
     }
