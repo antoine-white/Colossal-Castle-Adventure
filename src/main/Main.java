f4049c5af1839199208bde88c508c4c9c612a11d
antoine-white <40806880+antoine-white@users.noreply.github.com>
2019-12-04 15:19:08 +0100

    add +

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import game.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome in our game Collosal Castle Adventure.\nUse HELP command to get all available command\n"
+        System.out.println("Welcome in our game Collosal Castle Adventure.\nUse HELP command to get all available command\n" +
                            "You are an explorer in a massive haunted castle looking for a treasure...\n");
         Game g = new Game();
         g.runGame();        
